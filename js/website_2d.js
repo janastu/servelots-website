bfea5426f8491f1902b010b737e371e63cb095d2
Navya S Narayan <navyasn99@gmail.com>
2015-02-25 16:26:20 +0530

    Added link to Janastu site

--- js/website_2d.js
+++ js/website_2d.js
@@ -1,9 +1,18 @@
 function display (selected)
 {
-	document.getElementById("texty").innerHTML = "Text related to " + selected + " project.";
-	document.getElementById("texty").style.display="block";
-	document.getElementById("home").style.display="none";
-    
+	if (selected == 'Janastu')
+	{	
+		var option = "Janastu";
+		document.getElementById("texty").innerHTML = "Text related to " + selected + " project. <br/>" + option.link("http://janastu.org/");
+		document.getElementById("texty").style.display="block";
+		document.getElementById("home").style.display="none";
+	}
+	else 
+	{
+		document.getElementById("texty").innerHTML = "Text related to " + selected + " project.";
+		document.getElementById("texty").style.display="block";
+		document.getElementById("home").style.display="none";
+	}
 }
 
 function user (name)
